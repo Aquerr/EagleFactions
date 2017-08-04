2f0c60ecefc2b4b9eaeaa8daf58d926079de860f
Aquerr <nerdianin@gmail.com>
2017-08-04 22:41:11 +0200

    Clean up PlayerService

--- src/main/java/io/github/aquerr/eaglefactions/services/PlayerService.java
+++ src/main/java/io/github/aquerr/eaglefactions/services/PlayerService.java
@@ -1,6 +1,7 @@
 package io.github.aquerr.eaglefactions.services;
 
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.User;
 import org.spongepowered.api.service.user.UserStorageService;
 
@@ -14,17 +15,27 @@ public class PlayerService
 {
     public static Optional<String> getPlayerName(UUID playerUUID)
     {
+        Optional<User> oUser = getUser(playerUUID);
+
+        return Optional.of(oUser.get().getName());
+    }
+
+    public static Optional<Player> getPlayer(UUID playerUUID)
+    {
+        Optional<User> oUser = getUser(playerUUID);
+
+        return oUser.get().getPlayer();
+    }
+
+    private static Optional<User> getUser(UUID playerUUID)
+    {
         UserStorageService userStorageService = Sponge.getServiceManager().provideUnchecked(UserStorageService.class);
         Optional<User> oUser = userStorageService.get(playerUUID);
 
         if(oUser.isPresent())
         {
-            String name = oUser.get().getName();
-            return Optional.of(name);
+            return oUser;
         }
-        else
-        {
-            return Optional.empty();
-        }
+        else return Optional.empty();
     }
 }
