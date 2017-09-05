ec12b6d7475d6c87bd9adbf0fca88af1bc262938
Bartłomiej Stępień <bartlomiej.stepien@konsult.folksam.se>
2017-09-05 16:53:20 +0200

    Add two more checks for future work

--- src/main/java/io/github/aquerr/eaglefactions/listeners/EntitySpawnListener.java
+++ src/main/java/io/github/aquerr/eaglefactions/listeners/EntitySpawnListener.java
@@ -4,6 +4,7 @@ import io.github.aquerr.eaglefactions.EagleFactions;
 import io.github.aquerr.eaglefactions.logic.FactionLogic;
 import io.github.aquerr.eaglefactions.logic.MainLogic;
 import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.entity.living.monster.Monster;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
@@ -22,6 +23,10 @@ public class EntitySpawnListener
             if(entity instanceof Monster)
             {
                 EagleFactions.getEagleFactions().getLogger().info("Entity is a monster!!!! :O");
+                EagleFactions.getEagleFactions().getLogger().info("Printing enity type: " + entity.getType().toString());
+                EagleFactions.getEagleFactions().getLogger().info("Prining spawn couse: " + event.getCause().toString());
+
+
 
                 if(MainLogic.getMobSpawning() == false && FactionLogic.isClaimed(entity.getWorld().getUniqueId(), entity.getLocation().getChunkPosition()))
                 {
@@ -32,6 +37,8 @@ public class EntitySpawnListener
             else
             {
                 EagleFactions.getEagleFactions().getLogger().info("Entity is not a monster. :D");
+                EagleFactions.getEagleFactions().getLogger().info("Printing enity type: " + entity.getType().toString());
+                EagleFactions.getEagleFactions().getLogger().info("Prining spawn couse: " + event.getCause().toString());
             }
 
             //EntityType for CustomNPC (it counts as a monster) => EntityCustomNpc
