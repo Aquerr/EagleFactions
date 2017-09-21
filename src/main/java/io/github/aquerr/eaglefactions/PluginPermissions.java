0f1445123cf2a93c16d8972e7fe0527011511536
Bartłomiej Stępień <bartlomiej.stepien@konsult.folksam.se>
2017-09-21 15:36:09 +0200

    Add file that contains all plugin permissions

---
+++ src/main/java/io/github/aquerr/eaglefactions/PluginPermissions.java
@@ -0,0 +1,48 @@
+package io.github.aquerr.eaglefactions;
+
+public abstract class PluginPermissions
+{
+    public static final String AllPermissions = "eaglefactions";
+    public static final String AllCommands = "eaglefactions.command";
+
+    public static final String AllyCommands = "eaglefactions.command.ally";
+    public static final String AddAllyCommand = "eaglefactions.command.ally.add";
+    public static final String RemoveAllyCommand = "eaglefactions.command.ally.remove";
+
+    public static final String EnemyCommands = "eaglefactions.command.enemy";
+    public static final String AddEnemyCommand = "eaglefactions.command.enemy.add";
+    public static final String RemoveEnemyCommand = "eaglefactions.command.enemy.remove";
+
+    public static final String CreateCommand = "eaglefactions.command.create";
+    public static final String DisbandCommand = "eaglefactions.command.disband";
+    public static final String InviteCommand = "eaglefactions.command.invite";
+    public static final String JoinCommand = "eaglefactions.command.join";
+    public static final String LeaveCommand = "eaglefactions.command.leave";
+    public static final String ListCommand = "eaglefactions.command.list";
+    public static final String KickCommand = "eaglefactions.command.kick";
+    public static final String FriendlyFireCommand = "eaglefactions.command.friendlyfire";
+
+
+    public static final String HelpCommand = "eaglefactions.command.help";
+    public static final String PlayerCommand = "eaglefactions.command.player";
+    public static final String InfoCommand = "eaglefactions.command.info";
+    public static final String VersionCommand = "eaglefactions.command.version";
+
+    public static final String ClaimCommand = "eaglefactions.command.claim";
+    public static final String AutoClaimCommand = "eaglefactions.command.autoclaim";
+    public static final String UnclaimCommand = "eaglefactions.command.unclaim";
+    public static final String UnclaimAllCommand = "eaglefactions.command.unclaimall";
+
+    public static final String OfficerCommand = "eaglefactions.command.officer";
+    public static final String SetHomeCommand = "eaglefactions.command.sethome";
+    public static final String HomeCommand = "eaglefactions.command.home";
+
+    public static final String MapCommand = "eaglefactions.command.map";
+    public static final String AutoMapCommand = "eaglefactions.command.automap";
+
+    public static final String AdminCommand = "eaglefactions.command.admin";
+
+
+    public static final String SetPowerCommand = "eaglefactions.command.setpower";
+    public static final String MaxPowerCommand = "eaglefactions.command.maxpower";
+}
