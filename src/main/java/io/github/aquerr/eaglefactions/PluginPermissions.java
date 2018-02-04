32983217b7f70267ff070b49c2664ce9a7a2c4f6
Aquerr <nerdianin@gmail.com>
2018-02-04 13:43:19 +0100

    Updated permissions

--- src/main/java/io/github/aquerr/eaglefactions/PluginPermissions.java
+++ src/main/java/io/github/aquerr/eaglefactions/PluginPermissions.java
@@ -2,56 +2,58 @@ package io.github.aquerr.eaglefactions;
 
 public abstract class PluginPermissions
 {
-    public static final String AllPermissions = "eaglefactions";
-    public static final String AllCommands = "eaglefactions.command";
+    //public static final String AllPermissions = "eaglefactions";
 
-    public static final String AllyCommands = "eaglefactions.command.ally";
-    public static final String AddAllyCommand = "eaglefactions.command.ally.add";
-    public static final String RemoveAllyCommand = "eaglefactions.command.ally.remove";
+    //public static final String PlayerCommands = "eaglefactions.player";
+    //public static final String AdminCommands = "eaglefactions.admin";
 
-    public static final String EnemyCommands = "eaglefactions.command.enemy";
-    public static final String AddEnemyCommand = "eaglefactions.command.enemy.add";
-    public static final String RemoveEnemyCommand = "eaglefactions.command.enemy.remove";
+    public static final String AllyCommands = "eaglefactions.player.ally";
+    public static final String AddAllyCommand = "eaglefactions.player.ally.add";
+    public static final String RemoveAllyCommand = "eaglefactions.player.ally.remove";
 
-    public static final String CreateCommand = "eaglefactions.command.create";
-    public static final String DisbandCommand = "eaglefactions.command.disband";
-    public static final String InviteCommand = "eaglefactions.command.invite";
-    public static final String JoinCommand = "eaglefactions.command.join";
-    public static final String LeaveCommand = "eaglefactions.command.leave";
-    public static final String ListCommand = "eaglefactions.command.list";
-    public static final String KickCommand = "eaglefactions.command.kick";
-    public static final String FriendlyFireCommand = "eaglefactions.command.friendlyfire";
-    public static final String ChatCommand = "eaglefactions.command.chat";
-    public static final String TopCommand = "eaglefactions.command.top";
+    public static final String EnemyCommands = "eaglefactions.player.enemy";
+    public static final String AddEnemyCommand = "eaglefactions.player.enemy.add";
+    public static final String RemoveEnemyCommand = "eaglefactions.player.enemy.remove";
 
-
-    public static final String HelpCommand = "eaglefactions.command.help";
-    public static final String PlayerCommand = "eaglefactions.command.player";
-    public static final String InfoCommand = "eaglefactions.command.info";
-    public static final String InfoCommandSelf = "eaglefactions.command.info.self";
-    public static final String InfoCommandOthers = "eaglefactions.command.info.others";
-    public static final String VersionCommand = "eaglefactions.command.version";
-
-    public static final String ClaimCommand = "eaglefactions.command.claim";
-    public static final String AutoClaimCommand = "eaglefactions.command.autoclaim";
-    public static final String UnclaimCommand = "eaglefactions.command.unclaim";
-    public static final String UnclaimAllCommand = "eaglefactions.command.unclaimall";
-
-    public static final String SetLeaderCommand = "eaglefactions.command.setleader";
-    public static final String OfficerCommand = "eaglefactions.command.officer";
-    public static final String SetHomeCommand = "eaglefactions.command.sethome";
-    public static final String HomeCommand = "eaglefactions.command.home";
-
-    public static final String MapCommand = "eaglefactions.command.map";
-    public static final String AutoMapCommand = "eaglefactions.command.automap";
-
-    public static final String AdminCommand = "eaglefactions.command.admin";
+    public static final String CreateCommand = "eaglefactions.player.create";
+    public static final String DisbandCommand = "eaglefactions.player.disband";
+    public static final String InviteCommand = "eaglefactions.player.invite";
+    public static final String JoinCommand = "eaglefactions.player.join";
+    public static final String LeaveCommand = "eaglefactions.player.leave";
+    public static final String ListCommand = "eaglefactions.player.list";
+    public static final String KickCommand = "eaglefactions.player.kick";
+    public static final String FriendlyFireCommand = "eaglefactions.player.friendlyfire";
+    public static final String ChatCommand = "eaglefactions.player.chat";
+    public static final String TopCommand = "eaglefactions.player.top";
 
 
-    public static final String SetPowerCommand = "eaglefactions.command.setpower";
-    public static final String MaxPowerCommand = "eaglefactions.command.maxpower";
-    public static final String CoordsCommand = "eaglefactions.command.coords";
-    public static final String AttackCommand = "eaglefactions.command.attack";
+    public static final String HelpCommand = "eaglefactions.player.help";
+    public static final String PlayerCommand = "eaglefactions.player.player";
+    public static final String InfoCommand = "eaglefactions.player.info";
+    public static final String InfoCommandSelf = "eaglefactions.player.info.self";
+    public static final String InfoCommandOthers = "eaglefactions.player.info.others";
+    public static final String VersionCommand = "eaglefactions.player.version";
 
-    public static final String ReloadCommand = "eaglefactions.command.reload";
+    public static final String ClaimCommand = "eaglefactions.player.claim";
+    public static final String AutoClaimCommand = "eaglefactions.player.autoclaim";
+    public static final String UnclaimCommand = "eaglefactions.player.unclaim";
+    public static final String UnclaimAllCommand = "eaglefactions.player.unclaimall";
+
+    public static final String SetLeaderCommand = "eaglefactions.player.setleader";
+    public static final String OfficerCommand = "eaglefactions.player.officer";
+    public static final String SetHomeCommand = "eaglefactions.player.sethome";
+    public static final String HomeCommand = "eaglefactions.player.home";
+
+    public static final String MapCommand = "eaglefactions.player.map";
+    public static final String AutoMapCommand = "eaglefactions.player.automap";
+
+    public static final String CoordsCommand = "eaglefactions.player.coords";
+    public static final String AttackCommand = "eaglefactions.player.attack";
+
+    public static final String AdminCommand = "eaglefactions.admin.adminmode";
+
+    public static final String SetPowerCommand = "eaglefactions.admin.setpower";
+    public static final String MaxPowerCommand = "eaglefactions.admin.maxpower";
+
+    public static final String ReloadCommand = "eaglefactions.admin.reload";
 }
