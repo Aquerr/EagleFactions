307a9dc35f77fb671e8b0e8a412bce096a0f40f4
Aquerr <nerdianin@gmail.com>
2020-02-20 17:27:00 +0100

    Updated ProtectionManager test cases

---
+++ src/test/java/io/github/aquerr/eaglefactions/common/managers/ProtectionManagerTest.java
@@ -0,0 +1,108 @@
+package io.github.aquerr.eaglefactions.common.managers;
+
+import io.github.aquerr.eaglefactions.api.config.ChatConfig;
+import io.github.aquerr.eaglefactions.api.config.ProtectionConfig;
+import io.github.aquerr.eaglefactions.api.logic.FactionLogic;
+import io.github.aquerr.eaglefactions.api.managers.FlagManager;
+import io.github.aquerr.eaglefactions.api.managers.ProtectionManager;
+import io.github.aquerr.eaglefactions.common.managers.ProtectionManagerImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ProtectionManagerTest
+{
+	@Mock
+	private ChatConfig chatConfig;
+	@Mock
+	private ProtectionConfig protectionConfig;
+	@Mock
+	private FactionLogic factionLogic;
+	@Mock
+	private FlagManager flagManager;
+
+	@InjectMocks
+	private ProtectionManagerImpl protectionManager;
+
+	@Test
+	void bucketShouldBeWhitelisted()
+	{
+		//given
+		final Set<String> items = new HashSet<>();
+		items.add("minecraft:bucket");
+		when(protectionConfig.getWhiteListedItems()).thenReturn(items);
+		//when
+
+		final boolean result = protectionManager.isItemWhitelisted("minecraft:bucket");
+
+		//then
+		verify(protectionConfig).getWhiteListedItems();
+
+		assertTrue(result);
+	}
+
+	@Test
+	void bucketShouldNotBeWhitelisted()
+	{
+		final Set<String> items = new HashSet<>();
+		items.add("minecraft:bucket");
+
+		when(protectionConfig.getWhiteListedItems()).thenReturn(Collections.EMPTY_SET);
+
+		//then
+
+		final boolean result = protectionManager.isItemWhitelisted("minecraft:bucket");
+
+		verify(protectionConfig).getWhiteListedItems();
+		assertFalse(result);
+	}
+
+	@Test
+	void stoneShouldBeWhiteListedForPlaceAndDestroy()
+	{
+		//given
+		final Set<String> items = new HashSet<>();
+		items.add("minecraft:stone");
+		//when
+
+		when(protectionConfig.getWhiteListedPlaceDestroyBlocks()).thenReturn(items);
+
+		//then
+
+		final boolean result = protectionManager.isBlockWhitelistedForPlaceDestroy("minecraft:stone");
+
+		verify(protectionConfig).getWhiteListedPlaceDestroyBlocks();
+		assertTrue(result);
+	}
+
+	@Test
+	void stoneShouldNotBeWhiteListedForPlaceAndDestroy()
+	{
+		//given
+		//when
+		when(protectionConfig.getWhiteListedPlaceDestroyBlocks()).thenReturn(Collections.EMPTY_SET);
+
+		//then
+
+		final boolean result = protectionManager.isBlockWhitelistedForPlaceDestroy("minecraft:stone");
+
+		verify(protectionConfig).getWhiteListedPlaceDestroyBlocks();
+		assertFalse(result);
+	}
+}
