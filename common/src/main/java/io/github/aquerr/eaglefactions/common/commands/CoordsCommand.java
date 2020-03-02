830e0b2b2b548deb3eefa93e3654fd7ba0b3521c
Aquerr <nerdianin@gmail.com>
2020-03-02 23:18:01 +0100

    Show nearest faction claim in the coords command.

--- common/src/main/java/io/github/aquerr/eaglefactions/common/commands/CoordsCommand.java
+++ common/src/main/java/io/github/aquerr/eaglefactions/common/commands/CoordsCommand.java
@@ -1,9 +1,13 @@
 package io.github.aquerr.eaglefactions.common.commands;
 
+import com.flowpowered.math.vector.Vector3d;
+import com.flowpowered.math.vector.Vector3i;
 import io.github.aquerr.eaglefactions.api.EagleFactions;
+import io.github.aquerr.eaglefactions.api.entities.Claim;
 import io.github.aquerr.eaglefactions.api.entities.Faction;
 import io.github.aquerr.eaglefactions.common.PluginInfo;
 import io.github.aquerr.eaglefactions.common.messaging.Messages;
+import io.github.aquerr.eaglefactions.common.util.ParticlesUtil;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
@@ -16,10 +20,7 @@ import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
 import org.spongepowered.api.world.World;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 public class CoordsCommand extends AbstractCommand
 {
@@ -36,7 +37,7 @@ public class CoordsCommand extends AbstractCommand
         {
             if(!optionalFaction.isPresent())
                 throw new CommandException(Text.of(PluginInfo.ERROR_PREFIX, TextColors.RED, "You must specify faction name!"));
-            return showCoordsList(source, getTeamCoordsList(optionalFaction.get()));
+            return showCoordsList(source, getTeamCoordsList(null, optionalFaction.get()));
         }
         else
         {
@@ -46,7 +47,7 @@ public class CoordsCommand extends AbstractCommand
             {
                 final Faction faction = optionalFaction.get();
                 if(super.getPlugin().getPlayerManager().hasAdminMode(player) || (optionalPlayerFaction.isPresent() && optionalPlayerFaction.get().getName().equals(faction.getName())))
-                    return showCoordsList(player, getTeamCoordsList(faction));
+                    return showCoordsList(player, getTeamCoordsList(player, faction));
                 else throw new CommandException(Text.of(PluginInfo.ERROR_PREFIX, TextColors.RED, Messages.YOU_DONT_HAVE_ACCESS_TO_DO_THIS));
             }
 
@@ -54,13 +55,28 @@ public class CoordsCommand extends AbstractCommand
                 throw new CommandException(Text.of(PluginInfo.ERROR_PREFIX, TextColors.RED, Messages.YOU_MUST_BE_IN_FACTION_IN_ORDER_TO_USE_THIS_COMMAND));
 
             final Faction playerFaction = optionalPlayerFaction.get();
-            return showCoordsList(player, getTeamCoordsList(playerFaction));
+            return showCoordsList(player, getTeamCoordsList(player, playerFaction));
         }
     }
 
-    private List<Text> getTeamCoordsList(final Faction faction)
+    private List<Text> getTeamCoordsList(final Player player, final Faction faction)
     {
         final List<Text> teamCoords = new ArrayList<>();
+
+        //Get nearest claim if player is not null
+        if (player != null)
+        {
+            final Claim claim = getNearestClaim(player, faction);
+            if (claim != null)
+            {
+                final Vector3d blockPosition = ParticlesUtil.getChunkCenter(player.getWorld(), claim.getChunkPosition());
+                final Text textBuilder = Text.builder()
+                        .append(Text.of(Messages.FACTION + ": " + blockPosition))
+                        .build();
+
+                teamCoords.add(textBuilder);
+            }
+        }
 
         if(faction.getHome() != null)
         {
@@ -121,6 +137,33 @@ public class CoordsCommand extends AbstractCommand
             }
         }
         return teamCoords;
+    }
+
+    private Claim getNearestClaim(final Player player, final Faction faction)
+    {
+        final UUID worldUUID = player.getWorld().getUniqueId();
+        final Vector3i chunkPosition = player.getLocation().getChunkPosition();
+        final Set<Claim> claims = faction.getClaims();
+        if (claims.isEmpty())
+            return null;
+        
+        Claim nearestClaim = claims.iterator().next();
+
+        for (final Claim claim : claims)
+        {
+            if (!claim.getWorldUUID().equals(worldUUID))
+                continue;
+
+            float distanceToCurrentClaim = nearestClaim.getChunkPosition().distance(chunkPosition);
+            float distanceToNextClaim = claim.getChunkPosition().distance(chunkPosition);
+            if (distanceToNextClaim < distanceToCurrentClaim)
+                nearestClaim = claim;
+        }
+
+        if (!nearestClaim.getWorldUUID().equals(worldUUID))
+            nearestClaim = null;
+
+        return nearestClaim;
     }
 
     private CommandResult showCoordsList(final CommandSource commandSource, final List<Text> teamCoords) throws CommandException
