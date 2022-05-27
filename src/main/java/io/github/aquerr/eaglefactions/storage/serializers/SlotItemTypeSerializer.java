63667e3ceda5a3ed2788270016a7f2b52cacddd9
Aquerr <nerdianin@gmail.com>
2022-05-27 22:08:46 +0200

    Save item in chest as configurationnode instead of string

--- src/main/java/io/github/aquerr/eaglefactions/storage/serializers/SlotItemTypeSerializer.java
+++ src/main/java/io/github/aquerr/eaglefactions/storage/serializers/SlotItemTypeSerializer.java
@@ -6,38 +6,26 @@ import io.github.aquerr.eaglefactions.entities.FactionChestImpl;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.spongepowered.api.ResourceKey;
 import org.spongepowered.api.Sponge;
-import org.spongepowered.api.data.Keys;
 import org.spongepowered.api.data.persistence.DataContainer;
 import org.spongepowered.api.data.persistence.DataFormats;
 import org.spongepowered.api.data.persistence.DataQuery;
 import org.spongepowered.api.data.persistence.DataView;
 import org.spongepowered.api.item.ItemType;
-import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.inventory.ItemStack;
-import org.spongepowered.api.item.inventory.ItemStackComparators;
 import org.spongepowered.api.registry.RegistryEntry;
 import org.spongepowered.api.registry.RegistryTypes;
 import org.spongepowered.api.util.Tuple;
 import org.spongepowered.configurate.ConfigurationNode;
+import org.spongepowered.configurate.hocon.HoconConfigurationLoader;
 import org.spongepowered.configurate.serialize.SerializationException;
 import org.spongepowered.configurate.serialize.TypeSerializer;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-/**
- * This class has been mostly copied from Nuclues.
- *
- * <p>See https://github.com/NucleusPowered/Nucleus/blob/sponge-api/7/src/main/java/io/github/nucleuspowered/nucleus/configurate/typeserialisers/NucleusItemStackSnapshotSerialiser.java</p>
- *
- * <p>This class, as such, is copyrighted (c) by NucleusPowered team and Nucleus contributors.</p>
- */
 public class SlotItemTypeSerializer implements TypeSerializer<FactionChest.SlotItem>
 {
     @Override
@@ -47,96 +35,19 @@ public class SlotItemTypeSerializer implements TypeSerializer<FactionChest.SlotI
         final int row = node.node("row").getInt();
         final ConfigurationNode itemNode = node.node("item");
 
-        boolean emptyEnchant = false;
-        ConfigurationNode ench = itemNode.node("UnsafeData", "ench");
-        if (!ench.virtual())
-        {
-            List<? extends ConfigurationNode> enchantments = ench.childrenList();
-            if (enchantments.isEmpty())
-            {
-                // Remove empty enchantment list.
-                itemNode.node("UnsafeData").removeChild("ench");
-            }
-            else
-            {
-                enchantments.forEach(x -> {
-                    try
-                    {
-                        short id = Short.parseShort(x.node("id").getString());
-                        short lvl = Short.parseShort(x.node("lvl").getString());
-
-                        x.node("id").set(id);
-                        x.node("lvl").set(lvl);
-                    }
-                    catch (NumberFormatException | SerializationException e)
-                    {
-                        try
-                        {
-                            x.set(null);
-                        }
-                        catch (SerializationException ex)
-                        {
-                            ex.printStackTrace();
-                        }
-                    }
-                });
-            }
-        }
-
-        ConfigurationNode data = itemNode.node("Data");
-        if (!data.virtual() && data.isList())
-        {
-            List<? extends ConfigurationNode> n = data.childrenList().stream()
-                    .filter(x ->
-                            !x.node("DataClass").getString("").endsWith("SpongeEnchantmentData")
-                                    || (!x.node("ManipulatorData", "ItemEnchantments").virtual() && x.node("ManipulatorData", "ItemEnchantments").isList()))
-                    .collect(Collectors.toList());
-            emptyEnchant = n.size() != data.childrenList().size();
-
-            if (emptyEnchant)
-            {
-                if (n.isEmpty())
-                {
-                    itemNode.removeChild("Data");
-                }
-                else
-                {
-                    itemNode.node("Data").set(n);
-                }
-            }
-        }
-
         DataContainer dataContainer = null;
         try
         {
-            dataContainer = DataFormats.HOCON.get().read(itemNode.getString());
+            String itemNodeAsString = HoconConfigurationLoader.builder().buildAndSaveString(itemNode);
+            dataContainer = DataFormats.HOCON.get().read(itemNodeAsString);
         }
         catch (IOException e)
         {
             e.printStackTrace();
         }
-        Set<DataQuery> ldq = dataContainer.keys(true);
 
-        for (DataQuery dataQuery : ldq)
-        {
-            String el = dataQuery.asString(".");
-            if (el.contains("$Array$"))
-            {
-                try
-                {
-                    Tuple<DataQuery, Object> r = TypeHelper.getArray(dataQuery, dataContainer);
-                    dataContainer.set(r.first(), r.second());
-                }
-                catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
-
-                dataContainer.remove(dataQuery);
-            }
-        }
-
-        final Optional<ItemType> itemType = Sponge.game().registry(RegistryTypes.ITEM_TYPE).findEntry(ResourceKey.resolve(String.valueOf(dataContainer.get(DataQuery.of("ItemType")).get())))
+        final Optional<ItemType> itemType = Sponge.game().registry(RegistryTypes.ITEM_TYPE)
+                .findEntry(ResourceKey.resolve(String.valueOf(dataContainer.get(DataQuery.of("ItemType")).get())))
                 .map(RegistryEntry::value);
         if (!itemType.isPresent())
         {
@@ -158,19 +69,6 @@ public class SlotItemTypeSerializer implements TypeSerializer<FactionChest.SlotI
         {
             // don't bother
             throw new SerializationException("Could not deserialize item. Item is empty.");
-        }
-
-        if (emptyEnchant)
-        {
-            itemStack.offer(Keys.APPLIED_ENCHANTMENTS, new ArrayList<>());
-            return new FactionChestImpl.SlotItemImpl(column, row, itemStack);
-        }
-
-        if (itemStack.get(Keys.APPLIED_ENCHANTMENTS).isPresent())
-        {
-            // Reset the data.
-            itemStack.offer(Keys.APPLIED_ENCHANTMENTS, itemStack.get(Keys.APPLIED_ENCHANTMENTS).get());
-            return new FactionChestImpl.SlotItemImpl(column, row, itemStack);
         }
 
         return new FactionChestImpl.SlotItemImpl(column, row, itemStack);
@@ -217,7 +115,9 @@ public class SlotItemTypeSerializer implements TypeSerializer<FactionChest.SlotI
         node.node("row").set(obj.getRow());
         try
         {
-            node.node("item").set(DataFormats.HOCON.get().write(view));
+            String itemStackAsString = DataFormats.HOCON.get().write(view);
+            ConfigurationNode itemNode = HoconConfigurationLoader.builder().buildAndLoadString(itemStackAsString);
+            node.node("item").set(itemNode);
         }
         catch (IOException e)
         {
