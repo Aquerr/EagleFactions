f15edb4d14b6a0af4cf6a7a7a4e51e20c2b3bdf9
Aquerr <nerdianin@gmail.com>
2022-10-29 20:32:35 +0200

    Do not set ClaimOwners table charset. fixes #138

--- src/main/resources/assets/eaglefactions/queries/mysql/004 - Add AccessibleByFaction and Owners.sql
+++ src/main/resources/assets/eaglefactions/queries/mysql/004 - Add AccessibleByFaction and Owners.sql
@@ -6,7 +6,7 @@ CREATE TABLE `ClaimOwners` (
    `PlayerUUID`    VARCHAR(36)   NOT NULL,
    CONSTRAINT `ClaimOwners_Claim` FOREIGN KEY (`WorldUUID`, `ChunkPosition`) REFERENCES `Claims` (`WorldUUID`, `ChunkPosition`) ON DELETE CASCADE,
    CONSTRAINT `ClaimOwners_PlayerUUID` FOREIGN KEY (`PlayerUUID`) REFERENCES `Players` (`PlayerUUID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) DEFAULT CHARSET = utf8mb4;
+);
 
 -- Set database version to 4
 INSERT INTO Version VALUES (4);
