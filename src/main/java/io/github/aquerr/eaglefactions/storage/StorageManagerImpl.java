ab43c6da28d3738a3cec5bdd1d31f8a4072f33f3
Aquerr <nerdianin@gmail.com>
2024-06-17 22:01:55 +0200

    Make Storage Thread daemon thread.

--- src/main/java/io/github/aquerr/eaglefactions/storage/StorageManagerImpl.java
+++ src/main/java/io/github/aquerr/eaglefactions/storage/StorageManagerImpl.java
@@ -28,6 +28,7 @@ import io.github.aquerr.eaglefactions.storage.sql.sqlite.SqlitePlayerStorage;
 import io.github.aquerr.eaglefactions.storage.task.IStorageTask;
 import io.github.aquerr.eaglefactions.storage.task.StorageTaskFactory;
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -38,6 +39,7 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static java.lang.String.format;
@@ -48,7 +50,16 @@ public class StorageManagerImpl implements StorageManager
     private final PlayerStorage playerStorage;
     private final BackupStorage backupStorage;
 
-    private final ExecutorService executorService = Executors.newSingleThreadExecutor(); //Only one thread.
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor(new ThreadFactory()
+    {
+        @Override
+        public Thread newThread(@NotNull Runnable runnable)
+        {
+            Thread thread = new Thread(runnable);
+            thread.setDaemon(true);
+            return thread;
+        }
+    }); //Only one thread.
 
     private final StorageTaskFactory storageTaskFactory;
 
