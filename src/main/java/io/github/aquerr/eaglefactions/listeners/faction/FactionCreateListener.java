41a67645bed418c81ae0403b685d93276018f763
Aquerr <nerdianin@gmail.com>
2024-06-17 10:54:27 +0200

    Separate TabListUpdater logic + improvements for plugin loading

--- src/main/java/io/github/aquerr/eaglefactions/listeners/faction/FactionCreateListener.java
+++ src/main/java/io/github/aquerr/eaglefactions/listeners/faction/FactionCreateListener.java
@@ -3,7 +3,7 @@ package io.github.aquerr.eaglefactions.listeners.faction;
 import io.github.aquerr.eaglefactions.api.EagleFactions;
 import io.github.aquerr.eaglefactions.api.events.FactionCreateEvent;
 import io.github.aquerr.eaglefactions.listeners.AbstractListener;
-import io.github.aquerr.eaglefactions.scheduling.TabListUpdater;
+import io.github.aquerr.eaglefactions.tab.FactionTabListService;
 import org.spongepowered.api.entity.living.player.server.ServerPlayer;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
@@ -12,15 +12,21 @@ import org.spongepowered.api.util.Tristate;
 
 public class FactionCreateListener extends AbstractListener
 {
-    public FactionCreateListener(EagleFactions plugin)
+    private final FactionTabListService factionTabListService;
+
+    public FactionCreateListener(EagleFactions plugin, FactionTabListService factionTabListService)
     {
         super(plugin);
+        this.factionTabListService = factionTabListService;
     }
 
     @Listener(order = Order.POST)
     @IsCancelled(value = Tristate.FALSE)
     public void onFactionCreate(final FactionCreateEvent.Post event)
     {
-        new TabListUpdater(getPlugin().getConfiguration(), getPlugin().getPlayerManager()).updateTabListForPlayer((ServerPlayer) event.getCreator());
+        if (event.getCreator() != null)
+        {
+            factionTabListService.updateTabListForPlayer((ServerPlayer) event.getCreator());
+        }
     }
 }
--- src/main/java/io/github/aquerr/eaglefactions/listeners/faction/FactionKickListener.java
+++ src/main/java/io/github/aquerr/eaglefactions/listeners/faction/FactionKickListener.java
@@ -6,7 +6,7 @@ import io.github.aquerr.eaglefactions.api.entities.Faction;
 import io.github.aquerr.eaglefactions.api.entities.FactionPlayer;
 import io.github.aquerr.eaglefactions.api.events.FactionKickEvent;
 import io.github.aquerr.eaglefactions.listeners.AbstractListener;
-import io.github.aquerr.eaglefactions.scheduling.TabListUpdater;
+import io.github.aquerr.eaglefactions.tab.TabListUpdater;
 import net.kyori.adventure.text.Component;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.server.ServerPlayer;
